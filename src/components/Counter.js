bd5634d607824c3649bc83d380d9dbe554a05679
Ayesha Khan <ayeshanadeem9085@gmail.com>
2022-05-22 21:07:37 +0500

    minor change

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import React, { forwardRef } from "react";
 import "../styles/Counter.css"
 export const Counter = forwardRef((props, ref) => (
-    props.value !== 0 && console.log("component rendered!"),
+    console.log("component rendered!"),
     <input {...props} ref={ref} readOnly />
 ))
